f33384a86eff535b6a0e66244174cfff5bfb2ebe
cavalost <claudina.avalos@opendeusto.es>
2020-08-22 17:11:20 +0100

    test: add code for findSmallNums function

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -1,6 +1,8 @@
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
   // Your code here
+  if (!Array.isArray(nums)) throw new Error("nums must be an array");
+  return nums.filter(el => el < 1);
 }
 
 function findNamesBeginningWith(names, char) {
